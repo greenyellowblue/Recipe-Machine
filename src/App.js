f9da2659d586c351487424f7968eda7139e12a32
Michael <30552563+greenyellowblue@users.noreply.github.com>
2020-01-24 00:22:00 -0500

    Added Web App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Recipe from './Recipe';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const APP_ID = "cf685f3c";
+  const APP_KEY = "ddbfabeff33ca1a8d17c770bcaada2f8";
+
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState('chicken')
+
+
+  useEffect(() => {
+    getRecipes();
+  }, [query]);
+
+  const getRecipes = async () => {
+        const response  = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+        const data = await response.json();
+        setRecipes(data.hits);
+        console.log(data.hits);
+    };
+
+    const updateSearch = e => {
+        setSearch(e.target.value);
+        
+    };
+
+    const getSearch = e => {
+        e.preventDefault(); // Only search on submit button
+        setQuery(search);
+        setSearch('');
+    };
+
+  return( // Main Page
+    <div className = "App">
+      <form onSubmit = {getSearch} className="search-form">
+        <input className = "search-bar" type = "text" value = {search} onChange = {updateSearch}/>
+        <button className = "search-button" type = "submit">
+          Search
+        </button>
+      </form>
+      <div className = "recipes">
+      {recipes.map(recipe =>(
+          // Pass down information flow
+          <Recipe 
+            key = {recipe.recipe.label}
+            title ={recipe.recipe.label} 
+            calories = {"Total Calories: ".concat(Math.floor(recipe.recipe.calories))} // Stay healthy!
+            image = {recipe.recipe.image}
+            ingredients = {recipe.recipe.ingredients}
+          />
+      ))}
+      </div>
     </div>
   );
 }
